c286fff79b7a227397954d6cbc42b066d83ca508
DeanIsseyegh <issdea@gmail.com>
2017-08-28 14:39:11 +0100

    Updated .gtignore

---
+++ src/main/kotlin/dean/pushnotification/UserRepository.kt
@@ -0,0 +1,2 @@
+package dean.pushnotification
+
---
+++ src/test/kotlin/dean/pushnotification/GreetingControllerTest.kt
@@ -0,0 +1,16 @@
+package dean.pushnotification
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class GreetingControllerTest {
+    @Test
+    fun getCounter() {
+    }
+
+    @Test
+    fun greeting() {
+    }
+
+}
